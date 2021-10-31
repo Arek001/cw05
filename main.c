509cbe78ab4ccbb8fccfc1ef0650b2bdb7de07a9
Arek001 <59519837+Arek001@users.noreply.github.com>
2021-10-31 20:27:32 +0000

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+void Janie (void)
+{
+    printf("Panie Janie!\n");
+}
+
+void wstan (void)
+{
+    printf("Rano wstan\n");
+}
+
+int main()
+{
+    printf("Panie Janie!\n");
+    Janie();
+    wstan();
+    return 0;
+}
